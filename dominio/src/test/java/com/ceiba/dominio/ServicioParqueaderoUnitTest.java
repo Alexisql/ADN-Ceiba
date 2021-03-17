2abffe09c32fe0c9d8027080130675466d18788c
Alexisql <alexiskinte@gmail.com>
2021-03-16 20:55:21 -0500

    Se implementan nuevas pruebas unitarias para tener una mayor cobertura de codigo

--- dominio/src/test/java/com/ceiba/dominio/ServicioParqueaderoUnitTest.java
+++ dominio/src/test/java/com/ceiba/dominio/ServicioParqueaderoUnitTest.java
@@ -2,6 +2,7 @@ package com.ceiba.dominio;
 
 import com.ceiba.dominio.entidad.Carro;
 import com.ceiba.dominio.entidad.Moto;
+import com.ceiba.dominio.excepcion.PlacaNoPermitidaExcepcion;
 import com.ceiba.dominio.excepcion.SinCupoExcepcion;
 import com.ceiba.dominio.repositorio.CarroRepositorio;
 import com.ceiba.dominio.repositorio.MotoRepositorio;
@@ -11,6 +12,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.mockito.Mockito.when;
 
@@ -31,8 +37,6 @@ public class ServicioParqueaderoUnitTest {
 
     private ServicioParqueadero servicioParqueadero;
 
-    private String excepcionSinCupoMensaje;
-
     @Before
     public void inicializarVariables() {
         carroRepositorio = Mockito.mock(CarroRepositorio.class);
@@ -40,8 +44,16 @@ public class ServicioParqueaderoUnitTest {
         servicioParqueadero = new ServicioParqueadero(carroRepositorio, motoRepositorio);
     }
 
-    private void iniciarExcepcion(){
-        excepcionSinCupoMensaje = "No hay cupo disponible";
+    private String iniciarExcepcionSinCupo() {
+        return "No hay cupo disponible";
+    }
+
+    private Calendar fechaEntrada() throws ParseException {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        Calendar fechaIngreso = Calendar.getInstance();
+        Date fechaIngresoTemporal = formatoFecha.parse("08-03-2021 6:20:56");
+        fechaIngreso.setTime(fechaIngresoTemporal);
+        return fechaIngreso;
     }
 
     @Test
@@ -73,15 +85,29 @@ public class ServicioParqueaderoUnitTest {
     public void guardarCarro_sinCupoParaIngresar_exitoso() {
         //Arrange
         Carro carro = new Carro("AQW-5R4");
-        iniciarExcepcion();
         when(carroRepositorio.obtenerCantidadCarros()).thenReturn((byte) carro.CANTIDAD_MAXIMA_EN_PARQUEADERO);
         //Act
         try {
             servicioParqueadero.guardarCarro(carro);
-            fail();
         } catch (SinCupoExcepcion excepcion) {
             //Assert
-            assertEquals(excepcionSinCupoMensaje, excepcion.getMessage());
+            assertEquals(iniciarExcepcionSinCupo(), excepcion.getMessage());
+        }
+    }
+
+    @Test
+    public void guardarCarro_placaNoAutorizaDiaLunes_exitoso() throws ParseException {
+        //Arrange
+        Carro carro = new Carro("AQW-5R4");
+        carro.modificarFechaIngreso(fechaEntrada());
+        String excepcionPlacaNoPermitida = "No está autorizado a ingresar";
+        when(carroRepositorio.obtenerCantidadCarros()).thenReturn((byte) 10);
+        //Act
+        try {
+            servicioParqueadero.guardarCarro(carro);
+        } catch (PlacaNoPermitidaExcepcion excepcion) {
+            //Assert
+            assertEquals(excepcionPlacaNoPermitida, excepcion.getMessage());
         }
     }
 
@@ -89,15 +115,29 @@ public class ServicioParqueaderoUnitTest {
     public void guardarMoto_sinCupoParaIngresar_exitoso() {
         //Arrange
         Moto moto = new Moto("AQW-5R4", 650);
-        iniciarExcepcion();
         when(motoRepositorio.obtenerCantidadMotos()).thenReturn((byte) moto.CANTIDAD_MAXIMA_EN_PARQUEADERO);
         //Act
         try {
             servicioParqueadero.guardarMoto(moto);
-            fail();
         } catch (SinCupoExcepcion excepcion) {
             //Assert
-            assertEquals(excepcionSinCupoMensaje, excepcion.getMessage());
+            assertEquals(iniciarExcepcionSinCupo(), excepcion.getMessage());
+        }
+    }
+
+    @Test
+    public void guardarMoto_placaNoAutorizaDiaLunes_exitoso() throws ParseException {
+        //Arrange
+        Carro carro = new Carro("AQW-5R4");
+        carro.modificarFechaIngreso(fechaEntrada());
+        String excepcionPlacaNoPermitida = "No está autorizado a ingresar";
+        when(carroRepositorio.obtenerCantidadCarros()).thenReturn((byte) 10);
+        //Act
+        try {
+            servicioParqueadero.guardarCarro(carro);
+        } catch (PlacaNoPermitidaExcepcion excepcion) {
+            //Assert
+            assertEquals(excepcionPlacaNoPermitida, excepcion.getMessage());
         }
     }
 
--- dominio/src/test/java/com/ceiba/dominio/entidad/VehiculoTest.java
+++ dominio/src/test/java/com/ceiba/dominio/entidad/VehiculoTest.java
@@ -1,6 +1,5 @@
 package com.ceiba.dominio.entidad;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.text.ParseException;
@@ -12,27 +11,20 @@ import static org.junit.Assert.*;
 
 public class VehiculoTest {
 
-    private Calendar fechaIngreso;
-    private Calendar fechaSalida;
-
-    @Before
-    public void inicializarFechas() throws ParseException {
+    @Test
+    public void calcularTotalHorasEnParqueadero_diferancia5Horas_exitoso() throws ParseException {
+        //Arrange
         SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
         //Fecha de entrada
-        fechaIngreso = Calendar.getInstance();
+        Calendar fechaIngreso = Calendar.getInstance();
         String fechaIngresoFormato = "09-03-2021 6:20:56";
         Date fechaIngresoTemporal = formatoFecha.parse(fechaIngresoFormato);
         fechaIngreso.setTime(fechaIngresoTemporal);
         //Fecha de salida
-        fechaSalida = Calendar.getInstance();
+        Calendar fechaSalida = Calendar.getInstance();
         String fechaSalidaFormato = "09-03-2021 10:30:56";
         Date fechaSalidaTemporal = formatoFecha.parse(fechaSalidaFormato);
         fechaSalida.setTime(fechaSalidaTemporal);
-    }
-
-    @Test
-    public void calcularTotalHorasEnParqueadero_diferancia5Horas_exitoso() {
-        //Arrange
         Vehiculo vehiculo = new Vehiculo("AQS-JU8");
         vehiculo.modificarFechaIngreso(fechaIngreso);
         //Act
